de84b58bc116513628217a195498e2e108169d96
astawanzen <astawanzen@hotmail.com>
2022-10-01 06:38:50 +0800

    CallBackJS

---
+++ script.js
@@ -0,0 +1,17 @@
+/*CALLBACK = a function passed as an argument to another fuction or.... function is as your function argument
+
+dont add set of parentheses when you pass a function as an argument, here, displayDom is a function that we passed in
+sum(2, 3, displayConsole);
+function sum(x, y, yourCallBack) {
+  let result = x + y;
+  yourCallBack(result);
+}*/
+
+/*function displayConsole(output) {
+  console.log(output);
+}
+
+function displayDOM(output) {
+  document.getElementById("myLabel").innerHTML = output;
+}
+i still can't get my head around this!!!!*/
